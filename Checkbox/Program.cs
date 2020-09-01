8d10be1fb45a4b31ffba35fcab7d7a97f45a524a
frederikja163 <fjaplv64@gmail.com>
2020-09-01 13:48:47 +0200

    Sandbox projects

---
+++ Checkbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Checkbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TextOutput/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
