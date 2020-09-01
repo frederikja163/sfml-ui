585a486f93b06dd715ba519be596198ce644c9bd
frederikja163 <fjaplv64@gmail.com>
2020-09-01 14:13:57 +0200

    UI element base written

---
+++ SfmlUI/IUiElement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace SfmlUI
+{
+    public interface IUiElement
+    {
+        bool IsVisible { get; set; }
+        Vector2f Position { get; }
+        float Width { get;  }
+        float Height { get; }
+        Drawable DrawableShape { get; }
+       
+        void Draw();
+    }
+}
