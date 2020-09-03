3661d346650f1a1045c0343ca4d3e8e894ab85eb
Simon Stjernholm <simon@stjernholm.dev>
2020-09-03 08:21:30 +0200

    Create checkbox class

---
+++ SfmlUI/Checkbox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+
+namespace SfmlUI
+{
+    public class Checkbox : IUiElement
+    {
+
+        private bool _isVisible;
+
+        public bool IsVisible 
+        { 
+            get 
+            { 
+                return _isVisible; 
+            } 
+            set 
+            { 
+                _isVisible = value; 
+            } 
+        }
+
+        public Vector2f Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public float Width
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public float Height
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
