bad750b0151da40512f8183f7d0b45d7e47f8d6b
Mathias Sand <mathiassand14@gmail.com>
2020-09-08 13:37:35 +0200

    Sandbox button creating an instance of button

--- Button/Program.cs
+++ Button/Program.cs
@@ -1,22 +1,49 @@
 ﻿using System;
 using SFML.Graphics;
 using SFML.Window;
+using SFML.System;
+using SfmlUI;
 
-namespace Button
+namespace Button1
 {
+    
     class Program
     {
+        //private readonly SfmlUI.Button _button;
+        private static RenderWindow _window;
+        private static Button _button;
         static void Main(string[] args)
         {
-            RenderWindow window = new RenderWindow(VideoMode.DesktopMode, "Button test");
-            while (window.IsOpen)
+            _window = new RenderWindow(new VideoMode(1000, 1000), "Button");
+            _button = new Button();
+            _window.Closed += WindowClosed;
+
+            _window.SetFramerateLimit(60);
+            _window.SetVerticalSyncEnabled(true);
+            _button.IsVisible = true;
+            _button.Window = _window;
+            _button.Height = 400f;
+            _button.Width = 400f;
+            _button.Position = new Vector2f(_window.Size.X/2, _window.Size.Y/2);
+            
+
+
+
+            while (_window.IsOpen)
             {
-                window.DispatchEvents();
+                _window.DispatchEvents();
+                _window.Clear();
 
-                window.Clear();
 
-                window.Display();
+                _button.Draw();
+                _window.Display();
+                
             }
+        }
+
+        private static void WindowClosed(object sender, EventArgs e)
+        {
+            _window.Close();
         }
     }
 }
