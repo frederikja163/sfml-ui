d3f3336c221e1b6e93529cdcd59b14e16abdebf5
Simon Stjernholm <simon@stjernholm.dev>
2020-09-08 13:47:36 +0200

    blah

--- Checkbox/Program.cs
+++ Checkbox/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using SfmlUI;
 
-namespace Checkbox
+namespace CheckboxSandkasse
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RenderWindow window = new RenderWindow(VideoMode.DesktopMode, "Checkbox test");
+            window.SetFramerateLimit(30);
+            Checkbox checkbox = new Checkbox(window, new Vector2f(50, 50), 50, 50);
+            window.Closed += (_,__) => window.Close();
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+
+                window.Clear();
+                
+                checkbox.Draw();
+                
+                window.Display();
+            }
         }
     }
 }
--- SfmlUI/Checkbox.cs
+++ SfmlUI/Checkbox.cs
@@ -1,17 +1,28 @@
 ﻿using System;
 using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 
 namespace SfmlUI
 {
     public class Checkbox : IUiElement
     {
-        public Checkbox(Vector2f position, float width, float height, bool isChecked)
+        private RenderWindow _Window { get; }
+
+        public Checkbox(RenderWindow window, Vector2f position, float width, float height, bool isChecked = false, bool isVisible = true)
         {
+            _Window = window;
             Position = position;
             Width = width;
             Height = height;
             IsChecked = isChecked;
+            IsVisible = isVisible;
+            _Window.MouseButtonReleased += OnMouseButtonReleased;
+        }
+
+        private void OnMouseButtonReleased(object? sender, MouseButtonEventArgs e)
+        {
+            //Klik mus
         }
 
         public bool IsChecked { get; set; }
@@ -20,14 +31,21 @@ namespace SfmlUI
         public Vector2f Position { get; }
         public float Width { get; }
         public float Height { get; }
-        public void Draw() 
-        {
-            throw new NotImplementedException();
-        }
 
         public void Toggle()
         {
             IsChecked = !IsChecked;
+        }
+        public void Draw() 
+        {
+            if (IsVisible)
+            {
+                RectangleShape shape = new RectangleShape()
+                {
+                    FillColor = Color.White, Size = new Vector2f(Width, Height), Position = Position
+                };
+                _Window.Draw(shape);  
+            }
         }
     }
 }
