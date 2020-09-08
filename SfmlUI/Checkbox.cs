ddb7979d2e44712c1e2a96d41d3971161217f651
StjernholmPW <simon@stjernholm.dev>
2020-09-08 22:51:20 +0200

    Et eller andet

--- SfmlUI/Checkbox.cs
+++ SfmlUI/Checkbox.cs
@@ -48,27 +48,56 @@ namespace SfmlUI
 
                 if (IsChecked)
                 {
+                    // var offset = (MathF.Sqrt(2) * CrossThickness) / 2;
+                    // VertexArray vertexArray1 = new VertexArray(PrimitiveType.TriangleFan, 6);
+                    // vertexArray1[0] = new Vertex(new Vector2f(Position.X, Position.Y + Height - offset), CrossColor);
+                    // vertexArray1[1] = new Vertex(new Vector2f(Position.X, Position.Y + Height), CrossColor);
+                    // vertexArray1[2] = new Vertex(new Vector2f(Position.X + offset, Position.Y + Height), CrossColor);
+                    //
+                    // vertexArray1[3] = new Vertex(new Vector2f(Position.X + Width, Position.Y + offset), CrossColor);
+                    // vertexArray1[4] = new Vertex(new Vector2f(Position.X + Width, Position.Y), CrossColor);
+                    // vertexArray1[5] = new Vertex(new Vector2f(Position.X + Width - offset, Position.Y), CrossColor);
+                    //
+                    // VertexArray vertexArray2 = new VertexArray(PrimitiveType.TriangleFan, 6);
+                    // vertexArray2[0] = new Vertex(new Vector2f(Position.X, Position.Y + offset), CrossColor);
+                    // vertexArray2[1] = new Vertex(new Vector2f(Position.X, Position.Y), CrossColor);
+                    // vertexArray2[2] = new Vertex(new Vector2f(Position.X + offset, Position.Y), CrossColor);
+                    //
+                    // vertexArray2[3] = new Vertex(new Vector2f(Position.X + Width, Position.Y + Height - offset), CrossColor);
+                    // vertexArray2[4] = new Vertex(new Vector2f(Position.X + Width, Position.Y + Height), CrossColor);
+                    // vertexArray2[5] = new Vertex(new Vector2f(Position.X + Width - offset, Position.Y + Height), CrossColor);
+                    //
+                    // Window.Draw(vertexArray1);
+                    // Window.Draw(vertexArray2);
+                    
                     var offset = (MathF.Sqrt(2) * CrossThickness) / 2;
-                    VertexArray vertexArray1 = new VertexArray(PrimitiveType.TriangleFan, 6);
-                    vertexArray1[0] = new Vertex(new Vector2f(Position.X, Position.Y + Height - offset), CrossColor);
-                    vertexArray1[1] = new Vertex(new Vector2f(Position.X, Position.Y + Height), CrossColor);
-                    vertexArray1[2] = new Vertex(new Vector2f(Position.X + offset, Position.Y + Height), CrossColor);
+                    VertexArray vertexArray = new VertexArray(PrimitiveType.LineStrip, 16);
                     
-                    vertexArray1[3] = new Vertex(new Vector2f(Position.X + Width, Position.Y + offset), CrossColor);
-                    vertexArray1[4] = new Vertex(new Vector2f(Position.X + Width, Position.Y), CrossColor);
-                    vertexArray1[5] = new Vertex(new Vector2f(Position.X + Width - offset, Position.Y), CrossColor);
+                    vertexArray[0] = new Vertex(new Vector2f(Position.X, Position.Y + Height - offset), CrossColor);
+                    vertexArray[1] = new Vertex(new Vector2f(Position.X, Position.Y + Height), CrossColor);
+                    vertexArray[2] = new Vertex(new Vector2f(Position.X + offset, Position.Y + Height), CrossColor);
                     
-                    VertexArray vertexArray2 = new VertexArray(PrimitiveType.TriangleFan, 6);
-                    vertexArray2[0] = new Vertex(new Vector2f(Position.X, Position.Y + offset), CrossColor);
-                    vertexArray2[1] = new Vertex(new Vector2f(Position.X, Position.Y), CrossColor);
-                    vertexArray2[2] = new Vertex(new Vector2f(Position.X + offset, Position.Y), CrossColor);
+                    vertexArray[3] = new Vertex(new Vector2f(Position.X + Width/2, Position.Y + Height/2 + offset), CrossColor);
                     
-                    vertexArray2[3] = new Vertex(new Vector2f(Position.X + Width, Position.Y + Height - offset), CrossColor);
-                    vertexArray2[4] = new Vertex(new Vector2f(Position.X + Width, Position.Y + Height), CrossColor);
-                    vertexArray2[5] = new Vertex(new Vector2f(Position.X + Width - offset, Position.Y + Height), CrossColor);
+                    vertexArray[4] = new Vertex(new Vector2f(Position.X + Width - offset, Position.Y + Height), CrossColor);
+                    vertexArray[5] = new Vertex(new Vector2f(Position.X + Width, Position.Y + Height), CrossColor);
+                    vertexArray[6] = new Vertex(new Vector2f(Position.X + Width, Position.Y + Height - offset), CrossColor);
                     
-                    Window.Draw(vertexArray1);
-                    Window.Draw(vertexArray2);
+                    vertexArray[7] = new Vertex(new Vector2f(Position.X + Width/2 + offset, Position.Y + Height/2), CrossColor);
+                    
+                    vertexArray[8] = new Vertex(new Vector2f(Position.X + Width, Position.Y + offset), CrossColor);
+                    vertexArray[9] = new Vertex(new Vector2f(Position.X + Width, Position.Y), CrossColor);
+                    vertexArray[10] = new Vertex(new Vector2f(Position.X + Width - offset, Position.Y), CrossColor);
+                    
+                    vertexArray[11] = new Vertex(new Vector2f(Position.X + Width/2, Position.Y + Height/2 - offset), CrossColor);
+                    
+                    vertexArray[12] = new Vertex(new Vector2f(Position.X + offset, Position.Y), CrossColor);
+                    vertexArray[13] = new Vertex(new Vector2f(Position.X, Position.Y), CrossColor);
+                    vertexArray[14] = new Vertex(new Vector2f(Position.X, Position.Y + offset), CrossColor);
+                    
+                    vertexArray[15] = new Vertex(new Vector2f(Position.X + Width/2 - offset, Position.Y + Height/2), CrossColor);
+                    
+                    Window.Draw(vertexArray);
                     
                 }
             }
