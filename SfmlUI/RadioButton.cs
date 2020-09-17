8262482f2ecc9aade6b40deb08c650d30b154f70
Magnus Helmholt <magnus5405@outlook.com>
2020-09-17 10:17:07 +0200

    færdig?

--- SfmlUI/RadioButton.cs
+++ SfmlUI/RadioButton.cs
@@ -10,60 +10,130 @@ namespace SfmlUI
 {
     public class RadioButton : IUiElement
     {
-        public RadioButton(RenderWindow window, bool isSelected = false)
+        public RadioButton(RenderWindow window, Vector2f position, float radius, float lineSpacing, int amount, bool isSelected = false)
         {
             _window = window;
+            _position = position;
             _isSelected = isSelected;
+            _radius = radius;
+            _lineSpacing = lineSpacing;
+            _amount = amount;
+            _selected = -1;
             _window.MouseButtonReleased += OnMouseButtonReleased;
         }
 
         private bool _isSelected { get; set; }
 
+        private int _selected { get; set; }
+
         private RenderWindow _window { get; }
 
         public bool IsVisible { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public Vector2f Position => throw new NotImplementedException();
 
         public float Width => throw new NotImplementedException();
 
         public float Height => throw new NotImplementedException();
 
-        public void Draw()
+        private Vector2f _position;
+        public Vector2f Position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+            }
+        }
+
+        private float _radius;
+
+        public float Radius
+        {
+            get 
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value;
+            }
+        }
+
+        private float _lineSpacing;
+
+        public float lineSpacing
+        {
+            get
+            {
+                return _lineSpacing;
+            }
+            set
+            {
+                _lineSpacing = value;
+            }
+        }
+
+        private int _amount;
+
+        public int amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                _amount = value;
+            }
+        }
+
+        public void radioAmount()
+        { 
+        
+        }
+
+        public void Clear()
         {
             var rect = new RectangleShape(new Vector2f(1000, 1000));
             rect.FillColor = new Color(220, 220, 220);
             rect.Position = new Vector2f(0, 0);
 
-            var radio = new CircleShape(30);
-            radio.FillColor = new Color(255, 255, 255);
-            radio.OutlineThickness = 10;
-            radio.OutlineColor = new Color(0, 0, 0);
-            radio.Position = new Vector2f(_window.Size.X / 2, _window.Size.Y / 2);
-
-            var radioSelected = new CircleShape(17.5f);
-            radioSelected.FillColor = new Color(0, 0, 255);
-            radioSelected.OutlineThickness = 10;
-            radioSelected.OutlineColor = new Color(0, 0, 160);
-            radioSelected.Position = new Vector2f(radio.Position.X + 12, radio.Position.Y + 12);
-
             _window.Draw(rect);
+        }
 
-            _window.Draw(radio);
+        public void Draw()
+        {
+            for (int i = 0; i < amount; i++) { 
+                var radio = new CircleShape(_radius);
+                radio.FillColor = new Color(255, 255, 255);
+                radio.OutlineThickness = _radius/3;
+                radio.OutlineColor = new Color(0, 0, 0);
+                radio.Position = _position+i*new Vector2f(0,_lineSpacing);
 
-            if (_isSelected == true)
-            {
-                _window.Draw(radioSelected);
+                var radioSelected = new CircleShape(_radius/1.7f);
+                radioSelected.FillColor = new Color(0, 0, 255);
+                radioSelected.OutlineThickness = _radius/3;
+                radioSelected.OutlineColor = new Color(0, 0, 160);
+                radioSelected.Position = new Vector2f(radio.Position.X + _radius/2.4f, radio.Position.Y + _radius/2.4f);
+
+                _window.Draw(radio);
+
+                if (_selected == i)
+                {
+                    _window.Draw(radioSelected);
+                }
             }
-
         }
         private void OnMouseButtonReleased(object? sender, MouseButtonEventArgs e)
         {
-            if (_isSelected == false)
+            for (int i = 0; i < amount; i++)
             {
-                _isSelected = true;
-            } else {
-                _isSelected = false;
+                if (Math.Pow(e.X - _position.X - _radius, 2) + Math.Pow(e.Y - _position.Y * (i + 1) + i * lineSpacing - _radius, 2) <= Math.Pow(_radius, 2))
+                {
+                    _selected = i;
+                }
             }
         }
     }
