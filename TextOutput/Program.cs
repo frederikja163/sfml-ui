c19f2870cbd2fabf6dc600bdf7feb0792e571254
emilb <emilb@LAPTOP-4O2M6V7F>
2020-09-24 10:11:17 +0200

    textoutput

--- TextOutput/Program.cs
+++ TextOutput/Program.cs
@@ -16,7 +16,32 @@ namespace TextOutput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            //
+            //          private void InitializeComponent()
+            //{
+            //  this.label1 = new System.Windows.Forms.Label();
+            //  this.SuspendLayout();
+            // 
+            // label1
+            // 
+            // this.label1.AutoSize = true;
+            //  this.label1.Location = new System.Drawing.Point(88, 79);
+            //  this.label1.Name = "label1";
+            //   this.label1.Size = new System.Drawing.Size(89, 17);
+            //  this.label1.TabIndex = 0;
+            //  this.label1.Text = "hELLO world";
+            // 
+            // Form
+            // 
+            //   this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            //      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            //   this.ClientSize = new System.Drawing.Size(382, 353);
+            //       this.Controls.Add(this.label1);
+            //  this.Name = "Form";
+            //  this.Text = "Box";
+            //  this.ResumeLayout(false);
+            //      this.PerformLayout();
+
         }
     }
-}
+    }
