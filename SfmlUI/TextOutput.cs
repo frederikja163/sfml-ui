c1e5fcbf04bf76b4e5f665ba1c3f8a6439987741
BuildTools <unconfigured@null.spigotmc.org>
2020-09-24 15:35:21 +0200

    --

--- SfmlUI/TextOutput.cs
+++ SfmlUI/TextOutput.cs
@@ -7,31 +7,47 @@ namespace SfmlUI
     public sealed class TextBox : IUiElement
     {
         // Private variabler går her
+        private RenderWindow _window;
         private bool _isVisibble;
-        private Vector2f _position;
-        private float _width;
-        private float _height;
-        private Drawable _draw;
+        private Shape _box;
+        private Text _text;
 
 
         // Call metoder
         public bool IsVisible { get { return _isVisibble; } set { _isVisibble = value; } }
-        public Vector2f Position { get { return _position; } }
-        public float Width { get { return _width; } }
-        public float Height { get { return _height; } }
-        public Drawable DrawableShape { get { return _draw; } }
-
+        public Vector2f Position { get { return _box.Position; } }
+        public float Width { get { return _box.GetGlobalBounds().Width; } }
+        public float Height { get { return _box.GetGlobalBounds().Height; } }
 
         // Konstruktør
-        public TextBox(/* Her skal der argumenter (bool isVisible) */)
+        public TextBox(RenderWindow window, Vector2f position, float width, float height, Font font, uint fontSize)
         {
-            // _isVisible = isVisible
+            _window = window;
+            _isVisibble = true;
+            _box = new RectangleShape(new Vector2f(width, height)); // baggrund/box
+            _text = new Text("", font, fontSize); // text objekt til boxen
+            _box.Position = position;
+            _text.Position = position;
         }
 
         // Metoder/FunctionerS
-        public void Draw()
+        public void Draw() // metode til at tegne objektet
         {
-            throw new NotImplementedException();
+            if (_isVisibble)
+            {
+                _window.Draw(_box);
+                _window.Draw(_text);
+            }
+        }
+
+        public void Toggle() // metode til at slå synlighed til og fra
+        {
+
+        }
+
+        public void Output(string text) // metode til at ligge tekst ind i boxen
+        {
+            _text.DisplayedString = text;
         }
     }
 }
