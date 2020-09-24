5da45003c0b4ef7e5d8a5f324c32d23031a537db
Laczse <lassefonager@hotmail.com>
2020-09-24 16:39:18 +0200

    Movement of slider implementet. Value of slider implementet, but with slight problems

--- SfmlUI/Slider.cs
+++ SfmlUI/Slider.cs
@@ -1,37 +1,29 @@
 ﻿using SFML.Graphics;
 using SFML.System;
+using SFML.Window;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SfmlUI
 {
     public class Slider : IUiElement
     {
         private static RenderWindow _window;
-        public Slider(RenderWindow window, Vector2f Position,float Height,float Width, float Min, float Max)
+        public Slider(RenderWindow window, Vector2f Position, float Width, float Height, float Min, float Max)
         {
             _window = window;
             _position = Position;
+            _handlePos = Position;
             _height = Height;
             _width = Width;
             _min = Min;
             _max = Max;
             _isVisible = true;
-            //_isSelected = isSelected;
-            //_window.MouseButtonReleased += OnMouseButtonReleased;
+            _window.MouseButtonPressed += OnMouseButtonPressed;
+            _window.MouseMoved += OnMouseMoved;
+            _window.MouseButtonReleased += OnMouseButtonReleased;
         }
-        public RenderWindow Window
-        {
-            get
-            {
-                return _window;
-            }
-            set
-            {
-                _window = value;
-            }
-        }
+
+
         private bool _isVisible;
         public bool IsVisible
         {
@@ -57,8 +49,7 @@ namespace SfmlUI
                 _position = value;
             }
         }
-       
-
+        private Vector2f _handlePos;
         private float _min;
         public float Min
         {
@@ -108,8 +99,14 @@ namespace SfmlUI
                 _height = value;
             }
         }
-
-
+        private bool _clicked;
+        public float Value
+        {
+            get
+            {
+                return (_handlePos.X - Position.X) / _width;
+            }
+        }
 
         public void Draw()
         {
@@ -117,13 +114,14 @@ namespace SfmlUI
             back.FillColor = new Color(255, 255, 255);
             back.Position = new Vector2f(0, 0);
 
-            var rail = new RectangleShape(new Vector2f(_height,_width));
+            var rail = new RectangleShape(new Vector2f(_width, _height));
             rail.FillColor = new Color(220, 220, 220);
             rail.Position = _position;
 
-            var handle = new RectangleShape(new Vector2f(_height/10, _width));
-            handle.FillColor = new Color(100, 100, 100);            
-            handle.Position = _position;
+            var handle = new RectangleShape(new Vector2f(_width / 10, _height));
+            handle.FillColor = new Color(100, 100, 100);
+            handle.Position = _handlePos;
+            handle.Origin = new Vector2f(_width / 20f, 0f);
 
 
             if (_isVisible == true)
@@ -133,9 +131,53 @@ namespace SfmlUI
 
                 _window.Draw(rail);
                 _window.Draw(handle);
-
-
             }
         }
+        private void OnMouseButtonPressed(object? sender, MouseButtonEventArgs e)
+        {
+            _clicked = UpdateHandle(new Vector2f(e.X, e.Y));
+        }
+
+        private bool UpdateHandle(Vector2f mousePos)
+        {
+            float hwidth = _width / (2f * 10f);
+            if (mousePos.X >= _position.X + hwidth && mousePos.X <= _position.X + _width - hwidth &&
+                mousePos.Y >= _position.Y && mousePos.Y <= _position.Y + _height)
+            {
+                _handlePos = new Vector2f(mousePos.X, _position.Y);
+                Console.WriteLine(Value);
+                return true;
+            }
+            else if (mousePos.X >= _position.X && mousePos.X <= _position.X + hwidth &&
+                mousePos.Y >= _position.Y && mousePos.Y <= _position.Y + _height)
+            {
+                _handlePos = new Vector2f(_position.X + hwidth, _position.Y);
+                Console.WriteLine(Value);
+                return true;
+            }
+
+            else if (mousePos.X >= _position.X + _width - hwidth && mousePos.X <= _position.X + _width &&
+                mousePos.Y >= _position.Y && mousePos.Y <= _position.Y + _height)
+            {
+                _handlePos = new Vector2f(_position.X + _width - hwidth, _position.Y);
+                Console.WriteLine(Value);
+                return true;
+            }
+            return false;
+
+        }
+        private void OnMouseMoved(object sender, MouseMoveEventArgs e)
+        {
+            if (!_clicked)
+            {
+                return;
+
+            }
+            UpdateHandle(new Vector2f(e.X, _position.Y));
+        }
+        private void OnMouseButtonReleased(object sender, MouseButtonEventArgs e)
+        {
+            _clicked = false;
+        }
     }
-    }
+}
